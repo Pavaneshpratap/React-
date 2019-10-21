6c960a159d3d2381882dfbccc5089fe43b733f2f
Pavanesh Pratap Singh <pavanesh.pratap@appinventiv.com>
2019-10-21 11:14:16 +0530

    first commmit

---
+++ Student.js
@@ -0,0 +1,14 @@
+import React , {Component} from 'react';
+import {Text,View} from 'react-native';
+
+export default class Student extends React.Component {
+    render(){
+        return(
+            <View > 
+                <Text>
+                    Done Student
+                </Text>
+            </View>
+        );
+    }
+}
